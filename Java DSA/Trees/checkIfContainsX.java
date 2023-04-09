569f92f04d6362c7348d7650b5e4e71bdba38e52
Sachin Kumawat <121340987+Sachin958@users.noreply.github.com>
2023-04-09 21:13:42 +0530

    Create checkIfContainsX.java

---
+++ Java DSA/Trees/checkIfContainsX.java
@@ -0,0 +1,37 @@
+package Trees;
+
+public class checkIfContainsX {
+
+/*	TreeNode class
+ *
+ * class TreeNode<T> {
+		T data;
+		ArrayList<TreeNode<T>> children;
+
+		TreeNode(T data){
+			this.data = data;
+			children = new ArrayList<TreeNode<T>>();
+		}
+	}*/
+
+    public static boolean checkIfContainsX(TreeNode<Integer> root, int x){
+
+        // Write your code here
+        if(root == null){
+            return false;
+        }
+        if(root.data == x){
+            return true;
+        }else{
+            for(int i = 0; i< root.children.size(); i++){
+                if( checkIfContainsX(root.children.get(i), x)){
+                    return true;
+                }
+            }
+            return false;
+        }
+
+    }
+
+
+}
